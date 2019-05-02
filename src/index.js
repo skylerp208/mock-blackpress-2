3d3d2d13a40b2a6ee497184b59dc2a6469511356
Skyler Phillips <skylerwphillipstx@gmail.com>
2019-05-02 02:59:40 -0400

    About page mostly up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 window.addEventListener('DOMContentLoaded', (e) => {
+
+  // checks to see if user is browsing on mobile
   let isMobile = false
 
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   isMobile = true
   }
 
+  //if on mobile, adds an event listener
   if (isMobile === true ) {
     document.querySelector('#bars').addEventListener('click', (e) => {
       // document.querySelector('#bars').classList.toggle("clicked-bars")
@@ -20,6 +23,20 @@ window.addEventListener('DOMContentLoaded', (e) => {
       })
     })
   }
+
+  document.querySelector('#about-drop').addEventListener('click', (e) => {
+    e.preventDefault()
+    document.querySelector('#home-container').classList.toggle('hidden')
+    document.querySelector('#about-container').classList.toggle('hidden')
+    if ( document.querySelector('#dropdown').classList[0] === "clicked-dropdown" ) {
+        document.querySelector('#dropdown').classList.toggle('unclicked-dropdown')
+    } else {
+        document.querySelector('#dropdown').classList.toggle('clicked-dropdown')
+    }
+    document.querySelectorAll('.dropdown-item').forEach( (item) => {
+      item.classList.toggle('clicked-dropdown-item')
+    })
+  })
 
 
 })
