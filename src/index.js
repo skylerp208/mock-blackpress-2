d82d353888dc7c6b1e55af354805b9575481564a
Skyler Phillips <skylerwphillipstx@gmail.com>
2019-05-03 17:22:08 -0400

    menu done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('DOMContentLoaded', (e) => {
 
   // checks to see if user is browsing on mobile
@@ -24,18 +25,55 @@ window.addEventListener('DOMContentLoaded', (e) => {
     })
   }
 
+  //checks to see if container is hidden; if not, hides it
+  let hide = (container) => {
+    if (container.classList[0] === undefined) {
+      container.classList.toggle('hidden')
+    }
+  }
+
+  //changes a page from hidden to displayed, hides other pages
+  let openPage = (container) => {
+    let home = document.querySelector('#home-container')
+    let about = document.querySelector('#about-container')
+    let menu = document.querySelector('#menu-container')
+    let containers = [home, about, menu]
+      if (container.classList[0] === "hidden") {
+        container.classList.toggle('hidden')
+      }
+    containers.forEach( (cont) => {
+      if (cont!== container) {
+        hide(cont)
+      }
+    })
+    if ( document.querySelector('#dropdown').classList[0] === "clicked-dropdown" ) {
+          document.querySelector('#dropdown').classList.toggle('unclicked-dropdown')
+      } else {
+          document.querySelector('#dropdown').classList.toggle('clicked-dropdown')
+      }
+      document.querySelectorAll('.dropdown-item').forEach( (item) => {
+        item.classList.toggle('clicked-dropdown-item')
+      })
+
+  }
+
   document.querySelector('#about-drop').addEventListener('click', (e) => {
     e.preventDefault()
-    document.querySelector('#home-container').classList.toggle('hidden')
-    document.querySelector('#about-container').classList.toggle('hidden')
-    if ( document.querySelector('#dropdown').classList[0] === "clicked-dropdown" ) {
-        document.querySelector('#dropdown').classList.toggle('unclicked-dropdown')
-    } else {
-        document.querySelector('#dropdown').classList.toggle('clicked-dropdown')
-    }
-    document.querySelectorAll('.dropdown-item').forEach( (item) => {
-      item.classList.toggle('clicked-dropdown-item')
-    })
+    let about = document.querySelector('#about-container')
+    openPage(about)
+  })
+
+  document.querySelector('#home-link').addEventListener('click', (e) => {
+    e.preventDefault()
+    let home = document.querySelector('#home-container')
+    openPage(home)
+  })
+
+
+  document.querySelector('#menu-link').addEventListener('click' , (e) => {
+    e.preventDefault()
+    let menu = document.querySelector('#menu-container')
+    openPage(menu)
   })
 
 
